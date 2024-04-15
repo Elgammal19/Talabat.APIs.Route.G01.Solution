5ea6ceca3b579ff4e539e3a3e5123ecfd5c45089
Mohamed Elgammal <mohamadalgamal1919@gmail.com>
2024-04-15 10:27:10 +0200

    Generic Repository

---
+++ Talabat.Core/Repositories.Contract/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Entities;
+
+namespace Talabat.Core.Repositories.Contract
+{
+	public interface IGenericRepository<T> where T : BaseEntity
+	{
+		// GetAll
+		Task<IEnumerable<T>> GetAllAsync();
+
+		// GetrById
+		Task<T?> GetByIdAsync(int id);
+	}
+}

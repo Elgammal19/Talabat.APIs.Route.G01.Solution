ad3d9cd8bd3e2e28839a75e1f8cffab8c3a7e713
Mohamed Elgammal <mohamadalgamal1919@gmail.com>
2024-04-17 16:12:44 +0200

    Refactor GetProductById Using Specification

--- Talabat.APIs/Controllers/ProductController.cs
+++ Talabat.APIs/Controllers/ProductController.cs
@@ -36,7 +36,9 @@ namespace Talabat.APIs.Controllers
 		[HttpGet ("{id}")]
 		public async Task<ActionResult<Product>> GetProductById (int id)
 		{
-			var product = await _productRepo.GetByIdAsync (id);
+			var spec = new ProductWithBrandAndCategorySpecifications(id);
+
+			var product = await _productRepo.GetByIdWithSpecAsync (spec);
 
 			if (product is null)
 				return NotFound();
--- Talabat.Core/Specifications/Product Specs/ProductWithBrandAndCategorySpecifications.cs
+++ Talabat.Core/Specifications/Product Specs/ProductWithBrandAndCategorySpecifications.cs
@@ -9,8 +9,16 @@ namespace Talabat.Core.Specifications.Product_Specs
 {
 	public class ProductWithBrandAndCategorySpecifications : BaseSpecifications<Product>
 	{
-        public ProductWithBrandAndCategorySpecifications() : base()    // Create an Obj. --> Get All Products 
+		// Create an Obj. --> Get All Products 
+		public ProductWithBrandAndCategorySpecifications() : base()    
         {
+                Includes.Add(P => P.Brand);
+                Includes.Add(P => P.Category);
+        }
+
+		// Create an Obj. --> Get Product By Id 
+		public ProductWithBrandAndCategorySpecifications(int id) : base(P => P.Id == id) // Criteria        
+		{
                 Includes.Add(P => P.Brand);
                 Includes.Add(P => P.Category);
         }

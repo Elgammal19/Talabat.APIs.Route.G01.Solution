ae2704da690d89ddec8606d0528f9fddf6ec095a
Mohamed Elgammal <mohamadalgamal1919@gmail.com>
2024-04-17 16:32:49 +0200

    Draft Ex Emplyee &  Department Extensions Using Specification Pattern

---
+++ Talabat.APIs/Controllers/EmployeesController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Talabat.Core.Entities;
+using Talabat.Core.Repositories.Contract;
+using Talabat.Core.Specifications.Employee_Specs;
+
+namespace Talabat.APIs.Controllers
+{
+	public class EmployeesController : BaseApiController
+	{
+		private readonly IGenericRepository<Employee> _empRepo;
+
+		public EmployeesController(IGenericRepository<Employee> empRepo)
+        {
+			_empRepo = empRepo;
+		}
+
+		[HttpGet]
+		public async Task<ActionResult<IEnumerable<Employee>>> GetAllEmployees()
+		{
+			var spec = new EmployeeWithDepartmentSpecifications();
+			var emp = await _empRepo.GetAllWithSpecAsync(spec);
+
+			return Ok(emp);
+		}
+
+		[HttpGet("{id}")]
+		public async Task<ActionResult<Employee>> GetEmployeeById(int id)
+		{
+			var spec = new EmployeeWithDepartmentSpecifications(id);
+			var emp = await _empRepo.GetByIdWithSpecAsync(spec);
+
+			if (emp is null) return NotFound();
+
+			return Ok(emp);
+		}
+
+    }
+}

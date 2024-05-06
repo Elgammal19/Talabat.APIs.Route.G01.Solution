42eb0d31c0305536303d0258d628b80822327e37
Mohamed Elgammal <mohamadalgamal1919@gmail.com>
2024-05-06 04:29:15 +0300

    Order Module

---
+++ Talabat.Core/Entities/Order Aggregate/Address.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Talabat.Core.Entities.Order_Aggregate
+{
+	public class Address   // --> Composite Attribute
+	{
+		// required --> C# Keyword make the property can't be initialized with null
+		// && != [Required] --> Data Annotaion make this column rquired in DB & used for validation
+        public required string FirstName { get; set; } 
+        public string LastName { get; set; } = null!;
+        public string Street { get; set; } = null!;
+		public string City { get; set; } = null!;
+        public string Country { get; set; } = null!;
+	}
+}
---
+++ Talabat.Core/Entities/Order Aggregate/Order.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Talabat.Core.Entities.Order_Aggregate
+{
+	public class Order : BaseEntity
+	{
+		// To confirm this order is for this user by checking the email & id from token 
+        public string BuyerEmail { get; set; } = null!;
+
+		// To sole the issue of the differnce of the time in server time & user time when creating the order 
+		public DateTimeOffset OrderDate { get; set; } = DateTimeOffset.UtcNow;
+
+		public OrderStatus OrderStatus { get; set; } = OrderStatus.Pending;
+
+        public Address ShippingAddress { get; set; } = null!;
+
+		/// Foregin key for the DeliveryMethod tabele --> DeliveryMethod[one] : Order[Many]
+		/// To know which method order will be drived to the user
+		/// <summary>
+		/// public int DeliveryMethodId { get; set; } 
+		/// </summary>
+
+		// Navigational Property [one] --> For DeliveryMethod tabel
+		public DeliveryMethod DeliveryMethod { get; set; } = null!;
+
+		// Navigational Property [Many] --> For OrderItem tabel
+		public ICollection<OrderItem> Items { get; set; } = new HashSet<OrderItem>();
+
+		// Order Price without delivery cost
+        public decimal SubTotal { get; set; }
+
+        //public decimal Total => SubTotal + DeliveryMethod.Cost;
+
+		// Drived Attribute --> Will n;t be store in DB
+        public decimal GetTotal() => SubTotal + DeliveryMethod.Cost;
+
+        public string PaymentIntentId { get; set; } = string.Empty;
+
+    }
+}

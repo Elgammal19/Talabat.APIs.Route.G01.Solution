85d76254ef7a39c0fa3c018b101fab52361ecedb
Mohamed Elgammal <mohamadalgamal1919@gmail.com>
2024-05-10 02:00:58 +0300

    Product Service

--- Talabat.APIs/Controllers/ProductController.cs
+++ Talabat.APIs/Controllers/ProductController.cs
@@ -6,8 +6,10 @@ using Microsoft.AspNetCore.Mvc;
 using Talabat.APIs.DTOs;
 using Talabat.APIs.Errors;
 using Talabat.APIs.Helpers;
+using Talabat.Core;
 using Talabat.Core.Entities.Product;
 using Talabat.Core.Repositories.Contract;
+using Talabat.Core.Services.Contract;
 using Talabat.Core.Specifications;
 using Talabat.Core.Specifications.Product_Specs;
 using Talabat.Repository;
@@ -16,20 +18,24 @@ namespace Talabat.APIs.Controllers
 {
     public class ProductController : BaseApiController
 	{
-		private readonly IGenericRepository<Product> _productRepo;
-		private readonly IGenericRepository<ProductBrand> _brandRepo;
-		private readonly IGenericRepository<ProductCategory> _categoryRepo;
+		private readonly IProductService _productService;
+
+		///private readonly IGenericRepository<Product> _productRepo;
+		///private readonly IGenericRepository<ProductBrand> _brandRepo;
+		///private readonly IGenericRepository<ProductCategory> _categoryRepo;
 		private readonly IMapper _mapper;
 
 		// Inject the dependency of Product class that implement the IGenericRepository interface
-		public ProductController(IGenericRepository<Product> productRepo ,
-								 IGenericRepository<ProductBrand> brandRepo , 
-								 IGenericRepository<ProductCategory> categoryRepo,
-							     IMapper mapper )
+		public ProductController(IProductService productService,
+								 ///IGenericRepository<Product> productRepo ,
+								 ///IGenericRepository<ProductBrand> brandRepo , 
+								 ///IGenericRepository<ProductCategory> categoryRepo,
+								 IMapper mapper )
         {
-			_productRepo = productRepo;
-			_brandRepo = brandRepo;
-			_categoryRepo = categoryRepo;
+			_productService = productService;
+			///_productRepo = productRepo;
+			///_brandRepo = brandRepo;
+			///_categoryRepo = categoryRepo;
 			_mapper = mapper;
 		}
 
@@ -39,15 +45,9 @@ namespace Talabat.APIs.Controllers
 		[HttpGet]			// BaseUrl/api/Product --> GET method
         public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts([FromQuery]ProductSpecParams specParams)
 		{
-			var spec = new ProductWithBrandAndCategorySpecifications(specParams);
+			var products = await _productService.GetProductsAsync(specParams);
 
-			var products = await _productRepo.GetAllWithSpecAsync(spec);
-
-			////JsonResult result = new JsonResult(products);
-			//OkObjectResult result = new OkObjectResult(products);
-
-			var countSpec = new ProductWithFilterationsForCountSpecifications(specParams);
-			var count = await _productRepo.GetCountAsync(countSpec);
+			var count = await _productService.GetCountAsync(specParams);
 
 			var data = _mapper.Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(products); // --> Helper Method
 
@@ -61,9 +61,7 @@ namespace Talabat.APIs.Controllers
 		[ProducesResponseType(typeof(ApiResponse) ,404 /*StatusCodes.Status404NotFound*/)]
 		public async Task<ActionResult<ProductToReturnDto>> GetProductById (int id)
 		{
-			var spec = new ProductWithBrandAndCategorySpecifications(id);
-
-			var product = await _productRepo.GetByIdWithSpecAsync (spec);
+			var product = await _productService.GetProductAsync(id);
 
 			if (product is null)
 				return NotFound(new ApiResponse(404));
@@ -76,7 +74,7 @@ namespace Talabat.APIs.Controllers
 		public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetAllBrandsAsync()
 		{
 			// We are n't using specification here because there 's no navigational properties needed in Brand model
-			var brands = await _brandRepo.GetAllAsync(); 
+			var brands = await _productService.GetBrandsAsync(); 
 
 			return Ok(brands);
 		}
@@ -86,7 +84,7 @@ namespace Talabat.APIs.Controllers
 		public async Task<ActionResult<IReadOnlyList<ProductCategory>>> GetAllCategoriesAsync()
 		{
 			// We are n't using specification here because there 's no navigational properties needed in Brand model
-			var categories = await _categoryRepo.GetAllAsync();
+			var categories = await _productService.GetCategoriesAsync();
 
 			return Ok(categories);
 		} 
